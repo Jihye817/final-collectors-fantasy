3a94d1327918446204cdd50fe25f82c9f323f84f
Jihye <wlgul817@gmail.com>
2022-11-20 11:57:20 +0900

    Update Navbar.vue

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app-bar elevation="0" color="grey-lighten-3">
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    navbar test
+    <v-app-bar-title>navbar test</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-switch hide-details label="theme"></v-switch>
   </v-app-bar>
   <v-navigation-drawer v-model="drawer">
 
@@ -23,5 +25,10 @@ export default {
 </script>
 
 <style scoped>
-
+.v-col {
+  padding: 0px;
+}
+.v-row {
+  margin: 0px;
+}
 </style>
